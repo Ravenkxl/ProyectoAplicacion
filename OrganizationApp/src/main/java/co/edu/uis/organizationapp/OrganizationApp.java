c2251054ee5a2fa65a71d9d2d1a0d3bfcc4d4b63
Ravenkxl <karolhers07@gmail.com>
2025-04-24 17:16:16 -0500

    Creacion de Proyecto

---
+++ OrganizationApp/src/main/java/co/edu/uis/organizationapp/OrganizationApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.uis.organizationapp;
+
+/**
+ *
+ * @author Karol Hernandez
+ */
+public class OrganizationApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
